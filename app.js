95f5884d5e9de576031bc3166b7ad24adcfddac7
philiphe01 <luki.hertel@gmail.com>
2020-08-25 11:03:51 +0200

    Better structure, help embed

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const request   = require('request');
 require('dotenv').config({path: __dirname + '/.env'});
 bot.login(process.env.TOKEN);
 
+var commands = require('./commands.json');
 
 bot.on('ready', () =>{
     console.info(`Logged in as ${bot.user.tag}`);
@@ -15,77 +16,94 @@ bot.on('ready', () =>{
 const { OpusEncoder } = require('@discordjs/opus');
 const { toUnicode } = require('punycode');
 const { kMaxLength } = require('buffer');
+const { Z_NEED_DICT } = require('zlib');
  
 var isPlaying = false;
 
 bot.on('message', msg => {
     if(!msg.content.startsWith('!')) return;
+    if(msg.channel.name != 'bot') return;
     var message = msg.content.substring(1).toLowerCase();
-    switch (message) {
-        case 'gay':
-            //var user = msg.guild.members.random();
-            msg.channel.send('Nice');
-            break;
 
-        case 'insult':
-            https.get('https://insult.mattbas.org/api/en/insult.txt', (resp) => {
-                let data = '';
-                resp.on('data', (chunk) => {
-                    data += chunk;
-                });
-                resp.on('end', () => {
-                    msg.channel.send(data);
-                });
-            });
-            break;
-
-        case 'wetter':
-            const options = {
-                url: 'https://api.openweathermap.org/data/2.5/weather?lat=47.498&lon=8.278&appid=' + process.env.OPEN_WEATHER_API,
-                method: 'GET',
-                headers: {
-                    'Accept': 'application/json',
-                    'Accept-Charset': 'utf-8',
-                    'User-Agent': 'my-reddit-client'
-                }
-            };
-            
-            request(options, function(err, res, body) {
-                let json = JSON.parse(body);
-                msg.channel.send('Auf dem Tromsberg hat es ' + parseInt(json.main.temp - 273.15) + '°C und ' + json.weather[0].description );
-            });
-            break;
-
-        case 'stop':
-            msg.member.voice.channel.leave();
-            isPlaying = false;
-            break;
-        
-        case 'kollegah':
-            var track = './Audio/Kollegah' + Math.floor(Math.random() * 5) + '.mp3';
-            PlayMedia(msg, track, 0.3);
-            break;
-        
-        case 'hi':
-            PlayMedia(msg, './Audio/hi.mp3');
-            break;
-
-        case 'nice':
-            var track = './Audio/nice' + Math.floor(Math.random() * 6) + '.mp3';
-            PlayMedia(msg, track);
-            break;
-
+    var messageSent = false;
     
-        default:
-            msg.channel.send("Ah, ich hab' verkackt, mir ist egal");
-            PlayMedia(msg, './Audio/Verkackt.mp3', 0.3);
-	        break;
+    commands.forEach(command => {
+        if(command.name === message){
+            messageSent = true;
+            if(command.type === 'message') msg.channel.send(command.message);
+            else if(command.type === 'audio') PlayMedia(msg, command.file, command.volume);
+            else if(command.type === 'randomAudio'){
+                var number = Math.floor(Math.random() * command.file.length);
+                var volume = 0.5;
+                if (command.volume) volume = command.volume[number];
+                PlayMedia(msg, command.file[number], volume);
+            }
+        }
+    });
+
+    if(!messageSent){
+        switch (message) {
+            case 'insult':
+                https.get('https://insult.mattbas.org/api/en/insult.txt', (resp) => {
+                    let data = '';
+                    resp.on('data', (chunk) => {
+                        data += chunk;
+                    });
+                    resp.on('end', () => {
+                        msg.channel.send(data);
+                    });
+                });
+                break;
+
+            case 'wetter':
+                const options = {
+                    url: 'https://api.openweathermap.org/data/2.5/weather?lat=47.498&lon=8.278&appid=' + process.env.OPEN_WEATHER_API,
+                    method: 'GET',
+                    headers: {
+                        'Accept': 'application/json',
+                        'Accept-Charset': 'utf-8',
+                        'User-Agent': 'my-reddit-client'
+                    }
+                };
+                
+                request(options, function(err, res, body) {
+                    let json = JSON.parse(body);
+                    msg.channel.send('Auf dem Tromsberg hat es ' + parseInt(json.main.temp - 273.15) + '°C und ' + json.weather[0].description );
+                });
+                break;
+
+            case 'stop':
+                if(isPlaying) msg.member.voice.channel.leave();
+                isPlaying = false;
+                break;
+
+            case 'help':
+                var fields = [];
+                commands.forEach(command => {
+                    field = {'name': '!' + command.name, 'value': command.description};
+                    fields.push(field);
+                });
+                const helpEmbed = new Discord.MessageEmbed()
+                    .setColor('#0099ff')
+                    .setTitle('Nearly all Commands')
+                    .addFields(fields)
+                    .attachFiles(['./Images/embed.png'])
+                    .setThumbnail('attachment://embed.png')
+                msg.channel.send(helpEmbed);
+                break;
+        
+            default:
+                msg.channel.send("Ah, ich hab' verkackt, mir ist egal");
+                PlayMedia(msg, 'Verkackt.mp3', 0.3);
+                break;
+        }
     }
 });
 
 
 function PlayMedia(msg, file, volume = 0.5){
     var channel = msg.member.voice.channel;
+    file = './Audio/' + file;
     if(channel != null && isPlaying === false){
         channel.join().then(connection => {
             const dispatcher = connection.play(file);
